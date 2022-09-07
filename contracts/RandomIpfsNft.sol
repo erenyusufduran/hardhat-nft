4f81f14941b90ce75342bb1e467a0157a0371f9e
Eren Yusuf Duran <erenyusufduran1905@gmail.com>
2022-09-07 20:26:04 +0300

    mapping chainlink vrf requests

--- contracts/RandomIpfsNft.sol
+++ contracts/RandomIpfsNft.sol
@@ -3,11 +3,12 @@ pragma solidity ^0.8.7;
 
 import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 // When we mint an NFT, we will trigger a Chainlink VRF call to get us a random number,
 // using that number, we will get a random NFT
 
-contract RandomIpfsNft is VRFConsumerBaseV2 {
+contract RandomIpfsNft is VRFConsumerBaseV2, ERC721 {
   VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
   uint64 private immutable i_subscriptionId;
   bytes32 private immutable i_gasLane;
@@ -15,12 +16,18 @@ contract RandomIpfsNft is VRFConsumerBaseV2 {
   uint16 private constant REQUEST_CONFIRMATIONS = 3;
   uint32 private constant NUM_WORDS = 1;
 
+  // VRF Helpers
+  mapping(uint256 => address) public s_requestIdToSender;
+
+  // NFT Variables
+  uint256 public s_tokenCounter;
+
   constructor(
     address vrfCoordinatorV2,
     uint64 subscriptionId,
     bytes32 gasLane,
     uint256 callbackGasLimit
-  ) VRFConsumerBaseV2(vrfCoordinatorV2) {
+  ) VRFConsumerBaseV2(vrfCoordinatorV2) ERC721("Random IPFS NFT", "RIN") {
     i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
     i_subscriptionId = subscriptionId;
     i_gasLane = gasLane;
@@ -35,12 +42,17 @@ contract RandomIpfsNft is VRFConsumerBaseV2 {
       i_callbackGasLimit,
       NUM_WORDS
     );
+    s_requestIdToSender[requestId] = msg.sender;
   }
 
   function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords)
     internal
     override
-  {}
+  {
+    address dogOwner = s_requestIdToSender[requestId];
+    uint256 newTokenId = s_tokenCounter;
+    _safeMint(dogOwner, newTokenId);
+  }
 
-  function tokenURI(uint256) public {}
+  function tokenURI(uint256) public pure override returns(string memory) {}
 }
