22db68e1ec4e61f23658883ceea6a5499004bcfc
Eren Yusuf Duran <erenyusufduran1905@gmail.com>
2022-09-10 01:04:29 +0300

    deploy script

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -2,8 +2,7 @@ const networkConfig = {
   31337: {
     name: "localhost",
     ethUsdPriceFeed: "0x9326BFA02ADD2366b30bacB125260Af641031331",
-    gasLane:
-      "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc", // 30 gwei
+    gasLane: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc", // 30 gwei
     mintFee: "10000000000000000", // 0.01 ETH
     callbackGasLimit: "500000", // 500,000 gas
   },
@@ -11,21 +10,26 @@ const networkConfig = {
     name: "rinkeby",
     ethUsdPriceFeed: "0x8A753747A1Fa494EC906cE90E9f37563A8AF630e",
     vrfCoordinatorV2: "0x6168499c0cFfCaCD319c818142124B7A15E857ab",
-    gasLane:
-      "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc",
+    gasLane: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc",
     callbackGasLimit: "500000", // 500,000 gas
     mintFee: "10000000000000000", // 0.01 ETH
-    subscriptionId: "1002", // add your ID here!
+    subscriptionId: "10392", // add your ID here! vrf.chain.link
+  },
+  1: {
+    name: "mainnet",
+    keepersUpdateInterval: "30",
   },
 };
 
 const DECIMALS = "18";
 const INITIAL_PRICE = "200000000000000000000";
 const developmentChains = ["hardhat", "localhost"];
+const VERIFICATION_BLOCK_CONFIRMATIONS = 6;
 
 module.exports = {
   networkConfig,
   developmentChains,
   DECIMALS,
   INITIAL_PRICE,
+  VERIFICATION_BLOCK_CONFIRMATIONS,
 };
