66b426c9c17ae6f05d2354c31b44b68814b7faa0
Eren Yusuf Duran <erenyusufduran1905@gmail.com>
2022-09-14 18:13:06 +0300

    making the NFT dynamic

--- contracts/DynamicSvgNft.sol
+++ contracts/DynamicSvgNft.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.7;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import "base64-sol/base64.sol";
 
 contract DynamicSvgNft is ERC721 {
@@ -9,9 +10,20 @@ contract DynamicSvgNft is ERC721 {
   string private i_lowImageURI;
   string private i_highImageURI;
   string private constant base64EncodedSvgPrefix = "data:image/svg+xml;base64,";
+  AggregatorV3Interface internal immutable i_priceFeed;
+  mapping(uint256 => int256) public s_tokenIdToHighValue;
 
-  constructor(string memory lowSvg, string memory highSvg) ERC721("Dynamic SVG NFT", "DSN") {
+  event CreatedNFT(uint256 indexed tokenId, int256 highValue);
+
+  constructor(
+    address priceFeedAddress,
+    string memory lowSvg,
+    string memory highSvg
+  ) ERC721("Dynamic SVG NFT", "DSN") {
     s_tokenCounter = 0;
+    i_lowImageURI = svgToImgURI(lowSvg);
+    i_highImageURI = svgToImgURI(highSvg);
+    i_priceFeed = AggregatorV3Interface(priceFeedAddress);
   }
 
   function svgToImgURI(string memory svg) public pure returns (string memory) {
@@ -19,9 +31,11 @@ contract DynamicSvgNft is ERC721 {
     return string(abi.encodePacked(base64EncodedSvgPrefix, svgBase64Encoded));
   }
 
-  function mintNft() public {
+  function mintNft(int256 highValue) public {
+    s_tokenIdToHighValue[s_tokenCounter] = highValue;
     _safeMint(msg.sender, s_tokenCounter);
     s_tokenCounter = s_tokenCounter + 1;
+    emit CreatedNFT(s_tokenCounter, highValue);
   }
 
   function _baseURI() internal pure override returns (string memory) {
@@ -30,21 +44,28 @@ contract DynamicSvgNft is ERC721 {
 
   function tokenURI(uint256 tokenId) public view override returns (string memory) {
     require(_exists(tokenId), "URI Query for nonexistent token.");
-    string memory imageURI = "hi!";
-    return string(abi.encodePacked(
-      _baseURI(),
-      Base64.encode(
-        bytes(
-          abi.encodePacked(
-            '{"name":"',
-            name(),
-            '", "description": "An NFT that changes based on the Chainlink Feed", ',
-            '"attributes": [{"trait_type": "coolness", "value": 100}], "image":"',
-            imageURI,
-            '"}'
+    (, int256 price, , , ) = i_priceFeed.latestRoundData();
+    string memory imageURI = i_lowImageURI;
+    if (price >= s_tokenIdToHighValue[tokenId]) {
+      imageURI = i_highImageURI;
+    }
+    return
+      string(
+        abi.encodePacked(
+          _baseURI(),
+          Base64.encode(
+            bytes(
+              abi.encodePacked(
+                '{"name":"',
+                name(),
+                '", "description": "An NFT that changes based on the Chainlink Feed", ',
+                '"attributes": [{"trait_type": "coolness", "value": 100}], "image":"',
+                imageURI,
+                '"}'
+              )
+            )
           )
         )
-      )
-    ));
+      );
   }
 }
